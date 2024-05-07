1b2f8a38b8ef18bf339abe17326900e1753e8038
SilversMind <silversmind@gmail.com>
2024-05-07 22:56:15 +0200

    Rework page de profile

--- src/Profile/ProfilePage.tsx
+++ src/Profile/ProfilePage.tsx
@@ -1,53 +1,18 @@
-import {exerciseContainerStyle, exercisesContainerStyle, exerciseStyle, profileStyle,} from "./ProfileScreenStyle";
-import {useEffect, useState} from "react";
-import {settings} from "../setting";
-
-interface Profile {
-    name: string;
-    PR: {
-        bench: number;
-        squat: number;
-        deadlift: number;
-    };
-}
+import {useUser} from "../useUser";
+import {Avatar, Flex, Text} from "@chakra-ui/react";
+import React from "react";
 
 export const ProfilePage = () => {
-    const [ProfileData, setProfileData] = useState<Profile>();
+    const {currentUser} = useUser()
+    const filepath = `/assets/${currentUser}.jpeg`
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`${settings.defaultIPAddress}/profile/`);
-                const data = await response.json();
-                setProfileData(data);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
-        fetchData();
-    }, []);
-
-    if (!ProfileData) return <p>Loading</p>;
     return (
-        <>
-            <div style={profileStyle}>
-                <p>Hello {ProfileData?.name}</p>
-                <p>Vos PR:</p>
-                <div style={exercisesContainerStyle}>
-                    <div style={exerciseContainerStyle}>
-                        <p style={exerciseStyle}>Squat</p>
-                        <p style={exerciseStyle}>{ProfileData.PR.squat}</p>
-                    </div>
-                    <div style={exerciseContainerStyle}>
-                        <p style={exerciseStyle}>Bench</p>
-                        <p style={exerciseStyle}>{ProfileData.PR.bench}</p>
-                    </div>
-                    <div style={exerciseContainerStyle}>
-                        <p style={exerciseStyle}>Deadlift</p>
-                        <p style={exerciseStyle}>{ProfileData.PR.deadlift}</p>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+        <Flex direction={"column"} align={"center"} m={4}>
+            <Avatar size="2xl" name="Zouzou" src={filepath}/>
+            <Flex mt={2}>
+                <Text fontSize={"2xl"}>{currentUser}</Text>
+            </Flex>
+        </Flex>
+    )
+
 };
